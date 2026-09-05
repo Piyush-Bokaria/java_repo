97eed010911896b33e2351d08171a5d6ec085820
sachith balaka <sachithbalaka@gmail.com>
2026-09-05 22:22:48 +0530

    3rd line

--- Teacher.java
+++ Teacher.java
@@ -2,6 +2,7 @@ public class Teacher {
     public static void main(String[] args){
         System.out.println("Hello World !! Teacher");
         System.out.println("Bye Teacher");
+        System.out.println("3rd line");
     }
 
 }
