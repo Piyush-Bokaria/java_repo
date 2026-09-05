19f8a03d4227af0aa538f4e0cd817cb8f161e254
Piyush Bokaria <piyushbokaria215@gmail.com>
2026-09-05 22:19:51 +0530

    Random

--- Teacher.java
+++ Teacher.java
@@ -1,7 +1,7 @@
 public class Teacher {
     public static void main(String[] args){
         System.out.println("Hello World !! Teacher");
-        System.out.println("Hi Teacher");
+        System.out.println("Bye Teacher");
     }
 
 }
