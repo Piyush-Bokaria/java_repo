13602785219e0a50a08160041bd93ed9fb91e63c
sachith balaka <sachithbalaka@gmail.com>
2026-09-05 22:26:56 +0530

    add greet to student

--- Student.java
+++ Student.java
@@ -2,4 +2,7 @@ class Student{
     public static void main(String[] args){
         System.out.println("Hello World !!");
     }
+    public void greet(){
+        System.out.println("Good morning");
+    }
 }
